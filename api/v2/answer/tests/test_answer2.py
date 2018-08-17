acd8e44599cc263dd00484fabeb6c679acb95621
TheSteelGuy <collotuos0@gmail.com>
2018-08-17 11:53:24 +0300

    [finshes 159788421] add minimum test cases for ch3

---
+++ api/v2/answer/tests/test_answer2.py
@@ -0,0 +1,48 @@
+'''test question related user action'''
+import unittest
+import json
+from flask_testing import TestCase
+from flask import request
+from api.v2 import create_app
+from api.v2.answer.views import answers
+from api.v2.question.views import questions
+from .test_answer import Testbase
+
+
+class TestAnswer2(Testbase):
+    ''' test answer functionalities'''
+
+    def ask_question(self):
+        ''' help post a question for a testcase that needs it'''
+        res = self.client.post(
+            'api/v2/questions',
+            data=json.dumps(self.question),
+            content_type='application/json'
+        )
+        return res
+
+    def test_downvote_question(self):
+        '''test downvoting'''
+        self.ask_question()
+        answer = self.client.post(
+            'api/v2/questions/1/answers',
+            data=json.dumps(self.answer),
+            content_type='application/json'
+        )
+        self.assertEqual(answer.status_code, 201)
+        downvote = self.client.get(
+            'api/v2/questions/1/answers/1/downvote', content_type='application/json')
+        self.assertIn('-1', str(downvote.data))
+
+    def test_upvote_question(self):
+        '''test downvoting'''
+        self.ask_question()
+        answer = self.client.post(
+            'api/v2/questions/1/answers',
+            data=json.dumps(self.answer),
+            content_type='application/json'
+        )
+        self.assertEqual(answer.status_code, 201)
+        downvote = self.client.get(
+            'api/v2/questions/1/answers/1/upvote', content_type='application/json')
+        self.assertIn('1', str(downvote.data))
---
+++ api/v2/config.py
@@ -0,0 +1,40 @@
+''' configaration file for various environments such as production or testing'''
+import os
+
+class BaseConfig():
+    '''parent class subclassed by all other environ ment classes'''
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.urandom(24)
+
+
+class Development(BaseConfig):
+    '''class contains all configs relatedd to development enviroment'''
+    DEBUG = True
+    TESTING = True
+    os.environ['DB_NAME'] = 'sol'
+    os.environ['DB_USER'] = 'SOL'
+    os.environ['DB_PASSWORD'] = 'sol12h'
+    os.environ['DB_HOST'] = 'localhost'
+
+
+class Test(BaseConfig):
+    '''the class is used to run tests'''
+    TESTING = True
+    DEBUG = True
+    os.environ['DB_NAME'] = 'soltests'
+    os.environ['DB_USER'] = 'soladmin'
+    os.environ['DB_PASSWORD'] = 'sol12'
+    os.environ['DB_HOST'] = 'localhost'
+
+
+class Production(BaseConfig):
+    '''production configarations'''
+    TESTING = False
+
+
+CONFIG = {
+    'development': Development,
+    'testing': Test,
+    'production': Production
+}
