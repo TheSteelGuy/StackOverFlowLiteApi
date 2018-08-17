60504b9f6be5ac31c72d6eb82b9267ccd651c37f
TheSteelGuy <collotuos0@gmail.com>
2018-08-17 11:43:25 +0300

    [chore 159788233] set up  db

---
+++ api/v2/__init__.py
@@ -0,0 +1,32 @@
+'''
+___init__.py
+main config  file
+'''
+
+from flask_api import FlaskAPI
+from api.v2.config import CONFIG
+from api.v2.manage import DBSetup
+
+CONN = DBSetup()
+
+
+def create_app(config):
+    ''' function that receives configaration and creates the app'''
+    app = FlaskAPI(__name__)
+    app.config.from_object(CONFIG[config])
+    app.url_map.strict_slashes = False
+    from api.v2.answer.views import answer_blueprint
+    from api.v2.question.views import question_blueprint
+    from api.v2.comment.views import comment_blueprint
+    from api.v2.user.views import auth_blueprint
+    from . errors_handler import resource_not_found, method_not_allowed, server_error, bad_request
+    app.register_blueprint(answer_blueprint, url_prefix='/api/v2')
+    app.register_blueprint(question_blueprint, url_prefix='/api/v2')
+    app.register_blueprint(comment_blueprint, url_prefix='/api/v2')
+    app.register_blueprint(auth_blueprint, url_prefix='/api/v2/auth')
+    app.register_error_handler(404, resource_not_found)
+    app.register_error_handler(405,method_not_allowed)
+    app.register_error_handler(500, server_error)
+    app.register_error_handler(400, bad_request)
+    CONN.create_tables()
+    return app
---
+++ api/v2/tables.py
@@ -0,0 +1,42 @@
+''' module  that contains table definitions for both test db and api db'''
+def create():
+    tables_queries = (
+        'CREATE TABLE IF NOT EXISTS users (\
+                userId SERIAL PRIMARY KEY,\
+                username VARCHAR(40),\
+                email VARCHAR(64),\
+                confirmed_user BOOLEAN DEFAULT false,\
+                password VARCHAR(164)\
+                )',
+    
+        'CREATE TABLE IF NOT EXISTS questions (\
+                qid SERIAL PRIMARY KEY,\
+                title VARCHAR(55),\
+                body VARCHAR(255),\
+                authorId INTEGER REFERENCES users (userId) ON DELETE CASCADE\
+                )',
+        'CREATE TABLE IF NOT EXISTS answers (\
+                aId SERIAL PRIMARY KEY,\
+                description VARCHAR(250),\
+                votes INT,\
+                accepted BOOLEAN DEFAULT false,\
+                answer_date TIMESTAMP,\
+                questionId INTEGER REFERENCES questions (qId) ON DELETE CASCADE\
+                )',
+        'CREATE TABLE IF NOT EXISTS comments (\
+                cId SERIAL PRIMARY KEY,\
+                description VARCHAR(100),\
+                comment_date TIMESTAMP,\
+                answerId INTEGER REFERENCES answers (aId) ON DELETE CASCADE\
+                )',
+      
+        'CREATE TABLE IF NOT EXISTS tokens (\
+                tId SERIAL PRIMARY KEY,\
+                token VARCHAR(155)\
+                )'
+    
+    )
+        
+    return tables_queries
+
+   
